d13665b7b254ce037f6800d6fcf10ffd4f8059e8
pigman14 <62744504+pigman14@users.noreply.github.com>
2021-04-07 22:25:22 -0500

    Backup 4/7/2021 10:20 PM
    Some files are missing I think

---
+++ Web filter bypass/gogaurdian spammer/spamgenorator.py
@@ -0,0 +1,12 @@
+import random
+
+#open='window.open("'
+#url='url" ,"'
+#num= random.randint(300000, 90000000000000000000)
+#quot= '"'
+#par=")"
+#comma=','
+#print(open,url,num,quot,comma,quot,quot,par)
+x='2'
+for i in range(1, 11):
+    print(i,x)
